3614e5c4176e7ea35786bb48b5344014f7eb4c58
Kovenko Volodymyr <45285898+HikkaV@users.noreply.github.com>
2019-10-30 11:39:14 +0200

    Add files via upload

--- mobilenet_age_gender/net.py
+++ mobilenet_age_gender/net.py
@@ -1,33 +1,28 @@
 from keras.layers import Dropout, Conv2D, Dense, BatchNormalization, Flatten, MaxPooling2D, GlobalAveragePooling2D
 from keras.applications import MobileNet
 from settings import *
-from  keras import Model, Input
+from keras import Model, Input
 
 
 class DeepMN:
     def __init__(self, weights='imagenet'):
-        self.w=weights
+        self.w = weights
 
     def __call__(self):
-
-
-
         inputs = Input(shape=dim, name='the_input')
         model_mobilenet = MobileNet(input_shape=dim, alpha=1, depth_multiplier=1, dropout=1e-3,
                                     include_top=False, weights=self.w, input_tensor=None, pooling=None)
-
+        for i in model_mobilenet.layers[:10]:
+            i.trainable = True
         feat_a = model_mobilenet(inputs)
         feat_a = GlobalAveragePooling2D()(feat_a)
-        feat_a = Dropout(0.6)(feat_a)
-        feat_a = Dense(4000 , activation = 'relu')(feat_a)
-        feat_a = Dense(2000 , activation = 'relu')(feat_a)
+        feat_a = Dropout(0.5)(feat_a)
         pred_a_softmax = Dense(num_classes1, activation='softmax', name='age')(feat_a)
-        pred_g_softmax=Dense(2,activation='softmax',name='gender')(feat_a)
+        pred_g_softmax = Dense(2, activation='softmax', name='gender')(feat_a)
         model = Model(inputs=inputs, outputs=[pred_g_softmax, pred_a_softmax])
-        return model
 
+        return model
 
 
 if __name__ == '__main__':
     net = DeepMN()
-
